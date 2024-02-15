926bd9e685f9677adf8f0e3c30f2aa18d7f65d2e
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2024-02-16 00:10:12 +0300

    test

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from csv import QUOTE_NONE, unix_dialect, writer
+from datetime import datetime as dt
+
+from pep_parse.settings import BASE_DIR_TOTAL, RESULTS, DATETIME_FORMAT
+
+FILE_NAME = 'status_summary_{}.csv'
+
+class PepParsePipeline():
+
+    def __init__(self):
+        self.results = BASE_DIR_TOTAL / RESULTS
+        self.results.mkdir(exist_ok=True)
+        # RESULTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    def open_spider(self, spider):
+        self.counter = defaultdict(int)
+
+    def process_item(self, item, spider):
+        self.counter[item['status']] += 1
+        return item
+
+    def close_spider(self, spider):
+        with open(
+            self.results / FILE_NAME.format(
+                dt.now().strftime(DATETIME_FORMAT)
+            ),
+            mode='w',
+            encoding='utf-8',
+        ) as f:
+            writer(f, dialect=unix_dialect, quoting=QUOTE_NONE).writerows([
+                ('Status', 'Count'),
+                *self.counter.items(),
+                ('Total', sum(self.counter.values()))
+            ])
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        for peps_links in response.css('a.pep.reference.internal::attr(href)').getall():
+            yield response.follow(peps_links, callback=self.pep_parser)
+
+    def pep_parser(self, response):
+        string = response.css('h1.page-title::text').get().split(' ')
+        data = {
+            'number': int(string[1]),
+            'name': ' '.join(string[3:]),
+            'status': response.css('dt:contains("Status") + dd ::text').get()
+        }
+        yield PepParseItem(data)
+
+
