d5d2aaf4b248938bf8352da514c8cdfaeb8ab103
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2024-02-16 00:48:28 +0300

    fix flake8

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -1,7 +1,6 @@
 from scrapy import signals
 
 
-
 class PepParseSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
